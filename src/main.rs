db12ad50cef6db285bbc49d3484c6500a9ee215b
Patrick Gingras <775.pg.12@gmail.com>
2019-08-25 19:57:55 -0400

    trying some stuff out

--- src/main.rs
+++ src/main.rs
@@ -16,20 +16,21 @@ use value::Value;
 fn main() -> Result<(), String> {
     let mut agent = Agent::new();
 
-    let mut instructions = vec![OpCode::ConstString.into()];
-    instructions.extend_from_slice(&agent.intern_string("hello world").to_le_bytes());
-    instructions.push(OpCode::ConstNull.into());
-    instructions.push(OpCode::ConstTrue.into());
-    instructions.push(OpCode::ConstFalse.into());
-    instructions.push(OpCode::ConstDouble.into());
-    instructions.extend_from_slice(&1.23f64.to_bits().to_le_bytes());
+    let mut instructions = Vec::new();
     instructions.push(OpCode::ConstInt.into());
     instructions.extend_from_slice(&7474i64.to_le_bytes());
-    instructions.push(OpCode::Noop.into());
+    instructions.push(OpCode::ConstDouble.into());
+    instructions.extend_from_slice(&1.23f64.to_bits().to_le_bytes());
+    instructions.push(OpCode::Exp.into());
+
     let code_object = CodeObject::new(instructions);
 
     disassemble(&agent, &code_object)?;
 
     let mut interpreter = Interpreter::new(&mut agent);
-    interpreter.evaluate(code_object)
+    let result = interpreter.evaluate(code_object)?;
+
+    println!("\n{:?}", result);
+
+    Ok(())
 }
