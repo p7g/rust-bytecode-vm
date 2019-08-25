acbcdc9190f5400ad11406ec41cfea0c6e39e7fa
Patrick Gingras <775.pg.12@gmail.com>
2019-08-25 00:31:00 -0400

    added a disassembler

---
+++ src/disassemble.rs
@@ -0,0 +1,48 @@
+use std::convert::TryFrom;
+
+use crate::agent::Agent;
+use crate::code_object::CodeObject;
+use crate::opcode::OpCode;
+
+pub fn disassemble(agent: &Agent, code_object: &CodeObject) -> Result<(), String> {
+    let mut ip = 0;
+
+    macro_rules! next {
+        () => {{
+            let inst = code_object.instructions.get(ip);
+            ip += 1;
+            inst
+        }};
+        ($count:expr) => {{
+            let mut arr = [0u8; $count];
+
+            for i in 0..$count {
+                let result = next!().ok_or("Unexpected end of bytecode".to_string());
+                arr[i] = *result?;
+            }
+
+            arr
+        }};
+    }
+
+    while let Some(instruction) = next!() {
+        match OpCode::try_from(instruction)? {
+            instruction @ OpCode::ConstInt => {
+                println!("{:?}({:?})", instruction, i64::from_le_bytes(next!(8)));
+            },
+
+            instruction @ OpCode::ConstDouble => {
+                println!("{:?}({:?})", instruction, f64::from_bits(u64::from_le_bytes(next!(8))));
+            },
+
+            instruction @ OpCode::ConstString => {
+                println!("{:?}({:?})", instruction,
+                    agent.string_table[usize::from_le_bytes(next!(8))]);
+            },
+
+            instruction => println!("{:?}", instruction),
+        }
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod agent;
 mod code_object;
+mod disassemble;
 mod interpreter;
 mod module;
 mod opcode;
@@ -7,17 +8,28 @@ mod value;
 
 use agent::Agent;
 use code_object::CodeObject;
+use disassemble::disassemble;
 use interpreter::Interpreter;
 use opcode::OpCode;
 use value::Value;
 
 fn main() -> Result<(), String> {
-    let agent = Agent::new();
-    let mut interpreter = Interpreter::new(agent);
+    let mut agent = Agent::new();
 
-    let mut instructions = vec![OpCode::ConstInt.into()];
-    instructions.extend_from_slice(&1i64.to_le_bytes());
+    let mut instructions = vec![OpCode::ConstString.into()];
+    instructions.extend_from_slice(&agent.intern_string("hello world").to_le_bytes());
+    instructions.push(OpCode::ConstNull.into());
+    instructions.push(OpCode::ConstTrue.into());
+    instructions.push(OpCode::ConstFalse.into());
+    instructions.push(OpCode::ConstDouble.into());
+    instructions.extend_from_slice(&1.23f64.to_bits().to_le_bytes());
+    instructions.push(OpCode::ConstInt.into());
+    instructions.extend_from_slice(&7474i64.to_le_bytes());
+    instructions.push(OpCode::Noop.into());
     let code_object = CodeObject::new(instructions);
 
+    disassemble(&agent, &code_object)?;
+
+    let mut interpreter = Interpreter::new(&mut agent);
     interpreter.evaluate(code_object)
 }
