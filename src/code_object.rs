40f9c3efc39f9d64e398167d59cf3202bc1b24a1
Patrick Gingras <775.pg.12@gmail.com>
2019-08-25 14:59:27 -0400

    added function values

--- src/code_object.rs
+++ src/code_object.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub struct CodeObject {
     pub instructions: Vec<u8>,
 }
--- src/value.rs
+++ src/value.rs
@@ -1,3 +1,89 @@
+use crate::code_object::CodeObject;
+use crate::interpreter::Interpreter;
+
+type BuiltinFunction<'a> = fn(&mut Interpreter, &'a [Value<'a>]) -> Value<'a>;
+
+pub enum FunctionValue<'a> {
+    Builtin {
+        name: Option<usize>,
+        arity: usize,
+        function: BuiltinFunction<'a>,
+    },
+    User {
+        name: Option<usize>,
+        arity: usize,
+        code_object: CodeObject,
+    },
+}
+
+impl<'a> std::fmt::Debug for FunctionValue<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FunctionValue::Builtin {
+                name,
+                arity,
+                function,
+            } => write!(
+                f,
+                "FunctionValue::Builtin(name: {:?}, arity: {:?}, function: {:p})",
+                name, arity, function,
+            ),
+            FunctionValue::User {
+                name,
+                arity,
+                code_object,
+            } => write!(
+                f,
+                "FunctionValue::User(name: {:?}, arity: {:?}, code_object: {:?})",
+                name, arity, code_object,
+            ),
+        }
+    }
+}
+
+impl<'a> PartialEq for FunctionValue<'a> {
+    fn eq(&self, other: &FunctionValue<'a>) -> bool {
+        match self {
+            FunctionValue::Builtin {
+                name,
+                arity,
+                function,
+            } => {
+                if let FunctionValue::Builtin {
+                    name: other_name,
+                    arity: other_arity,
+                    function: other_function,
+                } = other
+                {
+                    name == other_name
+                        && arity == other_arity
+                        && function as *const BuiltinFunction
+                            == other_function as *const BuiltinFunction
+                } else {
+                    false
+                }
+            }
+            FunctionValue::User {
+                name,
+                arity,
+                code_object,
+            } => {
+                if let FunctionValue::User {
+                    name: other_name,
+                    arity: other_arity,
+                    code_object: other_code_object,
+                } = other {
+                    name == other_name &&
+                        arity == other_arity &&
+                        code_object == other_code_object
+                } else {
+                    false
+                }
+            }
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub enum Value<'a> {
     Integer(i64),
@@ -6,6 +92,7 @@ pub enum Value<'a> {
     Null,
     String(&'a str),
     Array(&'a [Value<'a>]),
+    Function(&'a FunctionValue<'a>),
 }
 
 impl<'a> Value<'a> {
@@ -17,6 +104,7 @@ impl<'a> Value<'a> {
             Value::Null => "null",
             Value::String(_) => "string",
             Value::Array(_) => "array",
+            Value::Function(_) => "function",
         }
     }
 }
@@ -48,6 +136,12 @@ impl<'a> From<&'a str> for Value<'a> {
 impl<'a> From<&'a Vec<Value<'a>>> for Value<'a> {
     fn from(vs: &'a Vec<Value<'a>>) -> Value<'a> {
         Value::Array(vs.as_slice())
+    }
+}
+
+impl<'a> From<&'a FunctionValue<'a>> for Value<'a> {
+    fn from(f: &'a FunctionValue<'a>) -> Value<'a> {
+        Value::Function(f)
     }
 }
 
@@ -89,5 +183,46 @@ mod tests {
         let v2 = Value::Array(vs.as_slice());
 
         assert_eq!(v1, v2);
+    }
+
+    fn builtin_function<'a>(_: &mut Interpreter, _: &'a [Value<'a>]) -> Value<'a> {
+        Value::Null
+    }
+
+    #[test]
+    fn test_functionvalue_equality() {
+        let a = FunctionValue::Builtin {
+            name: Some(123),
+            arity: 1,
+            function: builtin_function,
+        };
+        let b = FunctionValue::User {
+            name: Some(123),
+            arity: 1,
+            code_object: CodeObject::new(Vec::new()),
+        };
+        let c = FunctionValue::Builtin {
+            name: Some(123),
+            arity: 1,
+            function: builtin_function,
+        };
+        let d = FunctionValue::User {
+            name: Some(123),
+            arity: 1,
+            code_object: CodeObject::new(Vec::new()),
+        };
+
+        let av = Value::from(&a);
+        let bv = Value::from(&b);
+        let dv = Value::from(&d);
+
+        assert_ne!(a, b);
+        // might not be equal, function pointers can be different even when they point
+        // to the same function
+        // assert_eq!(a, c);
+        assert_eq!(b, d);
+
+        assert_ne!(av, bv);
+        assert_eq!(bv, dv);
     }
 }
