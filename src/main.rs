af634adebb07e596f5f138d2e48e93aeff6ab9d8
Patrick Gingras <775.pg.12@gmail.com>
2019-09-15 11:17:13 -0400

    added a bf interpreter

--- src/main.rs
+++ src/main.rs
@@ -458,13 +458,169 @@ function hashtable_new() {
 
 function hashtable_insert(table, key, value) {}
 
-let arr = dynamic_array_new();
 
-for let i = 0; i < 10000; i = i + 1 {
-    dynamic_array_push(arr, to_string(i));
+function tape_new() {
+    let arr = dynamic_array_new();
+    dynamic_array_push(arr, 0);
+    return [0, arr];
 }
 
-print(dynamic_array_to_array(arr));
+function tape_get(tape) {
+    return dynamic_array_get(tape[1], tape[0]);
+}
+
+function tape_get_char(tape) {
+    return chr(tape_get(tape));
+}
+
+function tape_inc(tape) {
+    let reel = tape[1];
+    let idx = tape[0];
+    dynamic_array_set(reel, idx, 1 + dynamic_array_get(reel, idx));
+}
+
+function tape_dec(tape) {
+    let reel = tape[1];
+    let idx = tape[0];
+    dynamic_array_set(reel, idx, dynamic_array_get(reel, idx) - 1);
+}
+
+function tape_advance(tape) {
+    tape[0] = tape[0] + 1;
+    if dynamic_array_length(tape[1]) <= tape[0] {
+        dynamic_array_push(tape[1], 0);
+    }
+}
+
+function tape_devance(tape) {
+    if tape[0] > 0 {
+        tape[0] = tape[0] - 1;
+    }
+}
+
+function bf_parse(program) {
+    let code = dynamic_array_new();
+    let bracket_map = assoc_list_new();
+    let left_stack = dynamic_array_new();
+    let pc = 0;
+
+    let chars = string_chars(program);
+    let len = array_length(chars);
+    let c;
+    let left;
+    for let i = 0; i < len; i = i + 1 {
+        c = chars[i];
+
+        if c == "[" {
+            dynamic_array_push(left_stack, pc);
+        } else if c == "]" {
+            left = dynamic_array_pop(left_stack);
+            if left != null {
+                assoc_list_set(bracket_map, left, pc);
+                assoc_list_set(bracket_map, pc, left);
+            }
+        } else if array_contains(string_chars("+-.,<>"), c) {
+        } else {
+            continue;
+        }
+
+        dynamic_array_push(code, c);
+        pc = pc + 1;
+    }
+
+    return [dynamic_array_to_array(code), bracket_map];
+}
+
+function bf_run(program) {
+    let code = program[0];
+    let bracket_map = program[1];
+    let tape = tape_new();
+    let len = array_length(code);
+    let pc = 0;
+
+    let op;
+    while pc < len {
+        op = code[pc];
+        if op == "+" {
+            tape_inc(tape);
+        } else if op == "-" {
+            tape_dec(tape);
+        } else if op == ">" {
+            tape_advance(tape);
+        } else if op == "<" {
+            tape_devance(tape);
+        } else if op == "[" {
+            if tape_get(tape) == 0 {
+                pc = assoc_list_get(bracket_map, pc);
+            }
+        } else if op == "]" {
+            if tape_get(tape) != 0 {
+                pc = assoc_list_get(bracket_map, pc);
+            }
+        } else if op == "." {
+            print(tape_get_char(tape));
+        }
+
+        pc = pc + 1;
+    }
+}
+
+let prog = "
+ Benchmark brainf*ck program
+>++[<+++++++++++++>-]<[[>+>+<<-]>[<+>-]++++++++
+[>++++++++<-]>.[-]<<>++++++++++[>++++++++++[>++
+++++++++[>++++++++++[>++++++++++[>++++++++++[>+
++++++++++[-]<-]<-]<-]<-]<-]<-]<-]++++++++++.
+";
+
+let hello = "
+[ This program prints \"Hello World!\" and a newline to the screen, its
+  length is 106 active command characters. [It is not the shortest.]
+
+  This loop is an \"initial comment loop\", a simple way of adding a comment
+  to a BF program such that you don't have to worry about any command
+  characters. Any '.', ',', '+', '-', '<' and '>' characters are simply
+  ignored, the '[' and ']' characters just have to be balanced. This
+  loop and the commands it contains are ignored because the current cell
+  defaults to a value of 0; the 0 value causes this loop to be skipped.
+]
+++++++++               Set Cell #0 to 8
+[
+    >++++               Add 4 to Cell #1; this will always set Cell #1 to 4
+    [                   as the cell will be cleared by the loop
+        >++             Add 2 to Cell #2
+        >+++            Add 3 to Cell #3
+        >+++            Add 3 to Cell #4
+        >+              Add 1 to Cell #5
+        <<<<-           Decrement the loop counter in Cell #1
+    ]                   Loop till Cell #1 is zero; number of iterations is 4
+    >+                  Add 1 to Cell #2
+    >+                  Add 1 to Cell #3
+    >-                  Subtract 1 from Cell #4
+    >>+                 Add 1 to Cell #6
+    [<]                 Move back to the first zero cell you find; this will
+                        be Cell #1 which was cleared by the previous loop
+    <-                  Decrement the loop Counter in Cell #0
+]                       Loop till Cell #0 is zero; number of iterations is 8
+
+The result of this is:
+Cell No :   0   1   2   3   4   5   6
+Contents:   0   0  72 104  88  32   8
+Pointer :   ^
+
+>>.                     Cell #2 has value 72 which is 'H'
+>---.                   Subtract 3 from Cell #3 to get 101 which is 'e'
++++++++..+++.           Likewise for 'llo' from Cell #3
+>>.                     Cell #5 is 32 for the space
+<-.                     Subtract 1 from Cell #4 for 87 to give a 'W'
+<.                      Cell #3 was set to 'o' from the end of 'Hello'
++++.------.--------.    Cell #3 for 'rl' and 'd'
+>>+.                    Add 1 to Cell #5 gives us an exclamation point
+>++.                    And finally a newline from Cell #6
+";
+
+let parsed = bf_parse(prog);
+bf_run(parsed);
 "#,
         );
         let parser = parser::Parser::new(&mut agent, lexer);
